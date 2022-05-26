a36613fba67ee5685a14ce019bd19e6111d56d96
Gozirin <chigozp2@gmail.com>
2022-05-26 18:32:00 +0100

    week 3 project

---
+++ app/src/main/java/com/example/preciouswk3task/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.preciouswk3task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Switch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)}}
+
+
